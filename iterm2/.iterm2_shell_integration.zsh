769a46700f651da42c2db3da850f589ccbcad55b
Jeff Widman <jeff@jeffwidman.com>
2017-09-19 16:40:11 -0700

    Update iterm2 Shell intregration

--- iterm2/.iterm2_shell_integration.zsh
+++ iterm2/.iterm2_shell_integration.zsh
@@ -1,6 +1,6 @@
-if [[ -o login ]]; then
+if [[ -o interactive ]]; then
   if [ "$TERM" != "screen" -a "$ITERM_SHELL_INTEGRATION_INSTALLED" = "" ]; then
-    export ITERM_SHELL_INTEGRATION_INSTALLED=Yes
+    ITERM_SHELL_INTEGRATION_INSTALLED=Yes
     ITERM2_SHOULD_DECORATE_PROMPT="1"
     # Indicates start of command output. Runs just before command executes.
     iterm2_before_cmd_executes() {
@@ -35,7 +35,7 @@ if [[ -o login ]]; then
     }
 
     # Mark start of prompt
-    iterm2_prompt_start() {
+    iterm2_prompt_mark() {
       printf "\033]133;A\007"
     }
 
@@ -89,7 +89,12 @@ if [[ -o login ]]; then
       ITERM2_SHOULD_DECORATE_PROMPT=""
 
       # Add our escape sequences just before the prompt is shown.
-      PS1="%{$(iterm2_prompt_start)%}$PS1%{$(iterm2_prompt_end)%}"
+      if [[ $PS1 == *'$(iterm2_prompt_mark)'* ]]
+      then
+        PS1="$PS1%{$(iterm2_prompt_end)%}"
+      else
+        PS1="%{$(iterm2_prompt_mark)%}$PS1%{$(iterm2_prompt_end)%}"
+      fi
     }
 
     iterm2_precmd() {
@@ -115,7 +120,11 @@ if [[ -o login ]]; then
     }
 
     # If hostname -f is slow on your system, set iterm2_hostname prior to sourcing this script.
-    [[ -z "$iterm2_hostname" ]] && iterm2_hostname=`hostname -f`
+    [[ -z "$iterm2_hostname" ]] && iterm2_hostname=`hostname -f 2>/dev/null`
+    # some flavors of BSD (i.e. NetBSD and OpenBSD) don't have the -f option
+    if [ $? -ne 0 ]; then
+      iterm2_hostname=`hostname`
+    fi
 
     [[ -z $precmd_functions ]] && precmd_functions=()
     precmd_functions=($precmd_functions iterm2_precmd)
@@ -124,6 +133,6 @@ if [[ -o login ]]; then
     preexec_functions=($preexec_functions iterm2_preexec)
 
     iterm2_print_state_data
-    printf "\033]1337;ShellIntegrationVersion=3;shell=zsh\007"
+    printf "\033]1337;ShellIntegrationVersion=5;shell=zsh\007"
   fi
 fi
